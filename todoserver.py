c0376e899af78ea3cec1abfd822cfdf3fb9a5367
Garrett Anderson <g@devnull.rip>
2023-03-23 16:16:59 -0700

    Add multiple tasks, setUP, etc

--- todoserver.py
+++ todoserver.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 MEMORY = {}
 
 
-def gen_tasks_id():
+def gen_task_id():
   task_id = random.getrandbits(64)
   if task_id in MEMORY:
-    task_id = gen_tasks_id()
+    task_id = gen_task_id()
   return task_id
 
 
@@ -30,7 +30,7 @@ def get_all_tasks():
 @app.route("/tasks/", methods=["POST"])
 def add_task():
   payload = request.get_json(force=True)
-  task_id = gen_tasks_id()
+  task_id = gen_task_id()
   MEMORY[task_id] = {
     "summary": payload["summary"],
     "description": payload["description"]
